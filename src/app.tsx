0287be385e1f0a21b91512e413c148544db419e5
IamDarya <daria.vyacheslavovna@gmail.com>
2021-08-03 23:21:02 +0200

    feat: add alll input elements to the form of registration

---
+++ src/app.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { FormOfRegistration } from "./form-of-registration/form-of-registration";
+
+export function App(): JSX.Element {
+  return (
+    <>
+      <h1>Form</h1>
+      <FormOfRegistration />
+    </>
+  );
+}
