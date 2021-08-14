6a99a5d217d9489356b74920191fafad0edcb8cc
IamDarya <75080122+IamDarya@users.noreply.github.com>
2021-08-15 01:59:15 +0200

    feat: add 'error' page

--- react-router/src/index.tsx
+++ react-router/src/index.tsx
@@ -1,14 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { HashRouter, Route } from "react-router-dom";
+import { HashRouter, Route, Switch } from "react-router-dom";
 import { About } from "./about/about";
 import { App } from "./app";
+import { ErrorPage } from "./error-page/error-page";
 // import reportWebVitals from './reportWebVitals';
 import "./style.scss";
 
 ReactDOM.render(
   <React.StrictMode>
     <HashRouter>
+      <Switch>
       <Route
         exact
         path='/'
@@ -19,6 +21,8 @@ ReactDOM.render(
         path='/about'
         component={About}
       />
+      <Route component={ErrorPage}/>
+      </Switch>
     </HashRouter>
   </React.StrictMode>,
   document.getElementById("root")
