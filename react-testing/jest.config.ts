ffc3251789eb7c88ea59d3bb1ddd05e893570460
IamDarya <75080122+IamDarya@users.noreply.github.com>
2021-08-24 22:04:39 +0200

    refactor: more configs

--- react-testing/jest.config.ts
+++ react-testing/jest.config.ts
@@ -4,6 +4,10 @@
  */
 
 export default {
+  "moduleNameMapper": {
+    "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/mocks/fileMock.ts",
+    "\\.(css|less|scss)$": "identity-obj-proxy"
+  },
   // All imported modules in your tests should be mocked automatically
   // automock: false,
 
